edac797dc81b2e5301c6c9f0147f7f8d3f592851
liyudong <1768756152@qq.com>
2022-08-17 19:04:16 +0800

    123

--- src/index.js
+++ src/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import './longin'
 import reportWebVitals from './reportWebVitals';
+import io from 'socket.io-client';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+const socket = io("ws://localhost:10001");
+socket.on('my_response', function(msg) {
+  console.log("msg", msg);
+});
+
+
+function App() {
+
+  socket.emit('my_event', {name: "lyd"});
+  return (
+    <div>
+     <h1>websocket.io</h1>
+    </div>
+  );
+}
+
+root.render(<App />);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
